2ffeec9398d34b261c0b3fc7ffc0f68094bedc7f
chuahongen <hong_enz@yahoo.com.sg>
2024-11-08 07:05:05 +0000

    Added new javascript functions

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function clearTextBox() {
+/*function clearTextBox() {
     document.getElementById("yourMessage").value="";
     event.preventDefault();
+}*/
+
+function testPhoneNum() {
+    /*var validPhoneNum = /^\+65(6|8|9)\d{7}$/;*/
+    var trueNum = "+6592397028";
+    var falsNum = "+6692397028";
+    /*console.log(validPhoneNum.test(trueNum));*/
+    console.log(trueNum);
 }
