80af7d363d4340539bc8faf1c27e23055eb9e6a1
chuahongen <hong_enz@yahoo.com.sg>
2024-11-08 08:24:36 +0000

    Added javascript functions to validate email addresses and phone numbers

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-function clearTextBox() {
-    document.getElementById("yourMessage").value="";
-    event.preventDefault();
+function testPhoneNum() {
+   var validPhoneNum = /^(6|8|9)\d{7}$/;
+   var phoneNum = document.getElementById("yourPhoneNum").value;
+   /*alert(validPhoneNum.test(phoneNum));*/
+   if(validPhoneNum.test(phoneNum)==false){
+    alert("Please enter a valid Singapore phone number");
+   }
 }
 
-function testPhoneNum() {
-   var validPhoneNum = /^\(6|8|9)\d{7}$/;
-   var phoneNum = document.getElementById("yourPhoneNum").value;
-   if (validPhoneNum.test(phoneNum)=false) {
-    window.alert("Please enter a valid Singapore phone number");
-   }
-    /* var trueNum = "+6592397028";
-    var falsNum = "+6692397028";
-    console.log(validPhoneNum.test(trueNum));
-    console.log(trueNum);*/
+function clearTextBox() {
+    document.getElementById("yourMessage").value="";
+    /*event.preventDefault();*/
+}
+
+function validateEmail() {
+    var validEmailAdd = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var emailAdd = document.getElementById("yourEmailAdd").value;
+    if(validEmailAdd.test(emailAdd)==false){
+        alert("Please enter a valid email address");
+    }
 }
