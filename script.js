03d00eeb6c71493d733c06fb3bf64ddc45ab798c
chuahongen <hong_enz@yahoo.com.sg>
2024-11-08 07:23:45 +0000

    Added Javascript functions

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-/*function clearTextBox() {
+function clearTextBox() {
     document.getElementById("yourMessage").value="";
     event.preventDefault();
-}*/
+}
 
 function testPhoneNum() {
-    /*var validPhoneNum = /^\+65(6|8|9)\d{7}$/;*/
-    var trueNum = "+6592397028";
+   var validPhoneNum = /^\(6|8|9)\d{7}$/;
+   var phoneNum = document.getElementById("yourPhoneNum").value
+   if (validPhoneNum.test(phoneNum)=false) {
+    window.alert(Please enter a valid Singapore phone number)
+   }
+    /* var trueNum = "+6592397028";
     var falsNum = "+6692397028";
-    /*console.log(validPhoneNum.test(trueNum));*/
-    console.log(trueNum);
+    console.log(validPhoneNum.test(trueNum));
+    console.log(trueNum);*/
 }
