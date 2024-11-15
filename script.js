6224233e27b1389db2f30a2d29810f5c8cb9ce9f
chuahongen <hong_enz@yahoo.com.sg>
2024-11-15 07:20:55 +0000

    Refined Javascripts

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function clearTextBox() {
     }
 }*/
 
-function validateEmail_PhoneNum() {
+function validateEmail_PhoneNum_Reg() {
     var validEmailAdd = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     var emailAdd = document.getElementById("yourEmailAdd").value;
     var validPhoneNum = /^(6|8|9)\d{7}$/;
@@ -35,7 +35,26 @@ function validateEmail_PhoneNum() {
         alert("Please enter a valid email address");
     } 
     else {
-        alert("Thank you for your interest. We will get back to you ASAP");
+        alert("Thank you for your registration. We will get back to you ASAP");
+    }
+}
+
+function validateEmail_PhoneNum_Contact() {
+    var validEmailAdd = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var emailAdd = document.getElementById("yourEmailAdd").value;
+    var validPhoneNum = /^(6|8|9)\d{7}$/;
+    var phoneNum = document.getElementById("yourPhoneNum").value;
+    if (validEmailAdd.test(emailAdd)==false && validPhoneNum.test(phoneNum)==false) {
+        alert("Please enter a valid email address and Singapore phone number");
+    }
+    else if (validEmailAdd.test(emailAdd)==true && validPhoneNum.test(phoneNum)==false) {
+        alert("Please enter a valid Singapore phone number");
+    }
+    else if (validEmailAdd.test(emailAdd)==false && validPhoneNum.test(phoneNum)==true) {
+        alert("Please enter a valid email address");
+    } 
+    else {
+        alert("Thank you for your query. We will get back to you ASAP");
     }
 }
 
@@ -44,7 +63,7 @@ function loadTechCourseDates() {
     var list2 = document.getElementById("tech_Courses_Date_Registration");
     var list1SelectedValue = list1.options[list1.selectedIndex].value;
     if (list1SelectedValue == "Incident_Response_and_Hacking_Tools") {
-        list2.options.length = 0;
+        list2.options.length = 3;
         list2.options[0] = new Option("--Select--", "Select");
         list2.options[1] = new Option("6 - 11 January 2025", "6_11_January_2025");
         list2.options[2] = new Option("3 - 8 February 2025", "3_8_February_2025");
